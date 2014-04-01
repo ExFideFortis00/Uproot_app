09b8186acebada351f176c5b36ea58cd2b8fd066
ExFideFortis00 <Xavier.O.Stewart@gmail.com>
2014-04-01 16:24:10 -0400

    More deletions to clear Heroku issues

--- db/migrate/20140328233509_add_timestamp_to_move_plans.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampToMovePlans < ActiveRecord::Migration
-  def change
-  	add_column :move_plans, :created_at, :datetime
-  	add_column :move_plans, :updated_at, :datetime
-  end	
-end
--- db/migrate/20140328234847_add_move_id_to_move_plans.rb
+++
@@ -1,5 +0,0 @@
-class AddMoveIdToMovePlans < ActiveRecord::Migration
-  def change
-  	add_column :move_plans, :move_id, :integer
-  end
-end
